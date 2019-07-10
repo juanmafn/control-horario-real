82627be03f6b1d61ecd1d1eab0de01f75d0bd50b
Juanma <juanmafn@gmail.com>
2019-07-10 19:01:49 +0200

    pruebas

--- SlitherioMods.user.js
+++ SlitherioMods.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Slither.io Mods
-// @namespace    http://slitherio.org/
+// @name         Slither.io Mods 123
+// @namespace    http://slitherio.org/ 123
 // @version      3.4
-// @description  Slither.io Mods by Slitherio.Org
-// @author       Slitherio.org
+// @description  Slither.io Mods by Slitherio.Org 123
+// @author       Slitherio.org 123
 // @match        http://slither.io/*
 // @match        https://slither.io/*
 // ==/UserScript==
