b68a7a4845f07ab7626c74ce6b1da0fe2ad16c57
Juanma <juanmafn@gmail.com>
2019-07-10 18:33:11 +0200

    Activar script sólo en jornada intensiva

--- ControlHorarioReal.js
+++ ControlHorarioReal.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Control horario correcto
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Debajo de las horas normales añado las horas teniendo en cuenta la jornada intensiva
 // @author       Juanma
 // @match        https://intranet.iti.upv.es/iti-hrm/controlhorario/
@@ -16,25 +16,25 @@ function getHoraStringFromHtml(horaHtml) {
 }
 
 function getSegundosFromHoraString(horaString) {
-	let h = +horaString.match(/(\d+):/)[1];
-	let m = +horaString.match(/:(\d+)/)[1];
+	const h = +horaString.match(/(\d+):/)[1];
+	const m = +horaString.match(/:(\d+)/)[1];
 
-	let d1 = new Date(2019, 1, 1, 0, 0);
-	let d2 = new Date(2019, 1, 1, h, m);
+	const d1 = new Date(2019, 1, 1, 0, 0);
+	const d2 = new Date(2019, 1, 1, h, m);
 
-	let signo = horaString.indexOf('-') >= 0 ? -1 : 1;
+	const signo = horaString.indexOf('-') >= 0 ? -1 : 1;
 
 	return (d2.getTime() - d1.getTime()) / 1000 * signo;
 }
 
 function getHoraHtmlFromSegundos(segundos, conColor, conSigno) {
-	let negativo = segundos < 0;
+	const negativo = segundos < 0;
 	if (negativo) segundos *= -1;
-	let h = parseInt(segundos / 3600);
+	const h = parseInt(segundos / 3600);
 	let m = parseInt((segundos % 3600) / 60);
 	if (m < 10) m = "0" + m;
 	let horaString = (negativo ? '-' : '+') + h + ':' + m;
-	let colorStyle = conColor ? ' style="color: ' + (negativo ? 'red' : 'green') + ';"' : '';
+	const colorStyle = conColor ? ' style="color: ' + (negativo ? 'red' : 'green') + ';"' : '';
 	horaString = conSigno ? horaString : horaString.replace('+', '').replace('-', '');
 	return '<span' + colorStyle + '>' + horaString + '</span>';
 }
@@ -49,20 +49,20 @@ function getCabecerasDOM() {
 }
 
 function getHorasEstipuladasEnSegundos() {
-    let horasEstipuladasHtml = $($('h3.no-margins')[1]).html();
-	let horasEstipuladas = getHoraStringFromHtml(horasEstipuladasHtml);
+	const horasEstipuladasHtml = $(getCabecerasDOM()[1]).html();
+	const horasEstipuladas = getHoraStringFromHtml(horasEstipuladasHtml);
 	return getSegundosFromHoraString(horasEstipuladas);
 }
 
 function getHorasEstipuladasAlFinalDelDiaEnSegundos() {
-	let horasEstipuladasFinalDelDiaHtml = $($('h3.no-margins')[2]).html();
-	let horasEstipuladasFinalDelDia = getHoraStringFromHtml(horasEstipuladasFinalDelDiaHtml);
+	const horasEstipuladasFinalDelDiaHtml = $(getCabecerasDOM()[2]).html();
+	const horasEstipuladasFinalDelDia = getHoraStringFromHtml(horasEstipuladasFinalDelDiaHtml);
 	return getSegundosFromHoraString(horasEstipuladasFinalDelDia);
 }
 
 function getDiferenciaEnSegundos() {
-    let horaFinalDiaHtml = $(getCabecerasDOM()[3]).html();
-	let horaFinalDia = getHoraStringFromHtml(horaFinalDiaHtml);
+	const horaFinalDiaHtml = $(getCabecerasDOM()[3]).html();
+	const horaFinalDia = getHoraStringFromHtml(horaFinalDiaHtml);
 	return getSegundosFromHoraString(horaFinalDia);
 }
 
@@ -71,14 +71,14 @@ function getDiasLaborablesRestantes() {
 }
 
 function getHorasAlDiaHastaFinDeMesEnSegundos() {
-    let horasAlDiaHtml = $(getCabecerasDOM()[5]).html();
-	let horasAlDia = getHoraStringFromHtml(horasAlDiaHtml);
+	const horasAlDiaHtml = $(getCabecerasDOM()[5]).html();
+	const horasAlDia = getHoraStringFromHtml(horasAlDiaHtml);
 	return getSegundosFromHoraString(horasAlDia);
 }
 
 function getSaldoCerditoEnSegundos() {
-    let horasSaldoCerditoHtml = $(getCabecerasDOM()[6]).html();
-	let horasSaldoCerdito = getHoraStringFromHtml(horasSaldoCerditoHtml);
+	const horasSaldoCerditoHtml = $(getCabecerasDOM()[6]).html();
+	const horasSaldoCerdito = getHoraStringFromHtml(horasSaldoCerditoHtml);
 	return getSegundosFromHoraString(horasSaldoCerdito);
 }
 
@@ -111,8 +111,8 @@ function setHoraSaldo(htmlHoraSaldo) {
 }
 
 function setAclaracion(descuentoSegundosCerditoAlDia, segundosXDia) {
-	let descuentoHorasCerditoAlDia = getHoraHtmlFromSegundos(descuentoSegundosCerditoAlDia, false, false);
-	let horasXDia = getHoraHtmlFromSegundos(segundosXDia, false, false);
+	const descuentoHorasCerditoAlDia = getHoraHtmlFromSegundos(descuentoSegundosCerditoAlDia, false, false);
+	const horasXDia = getHoraHtmlFromSegundos(segundosXDia, false, false);
 	$($('.row.nopadding.widgets')[0]).after(`
 		<div class="row nopadding widgets">
 			<div class="col-md-12" style="padding-right: 7px;">
@@ -142,6 +142,11 @@ function setAclaracion(descuentoSegundosCerditoAlDia, segundosXDia) {
 
 /*** Lógica ***/
 
+function esEpocaDeJornadaIntensiva() {
+	const hoy = new Date();
+	return [6, 7].includes(hoy.getMonth());
+}
+
 function getDiasLaborables(horasEstipuladasEnSegundos) {
 	return horasEstipuladasEnSegundos / (7.5 * 3600);
 }
@@ -152,10 +157,6 @@ function getSegundosARestarParaJornadaIntensiva(diasLaborables, saldoCerditoEnSe
 		segundosARestarParaJornadaIntensiva = saldoCerditoEnSegundos;
 	}
 	return segundosARestarParaJornadaIntensiva;
-}
-
-function getNumDiasTrabajados(diasLaborables) {
-	return diasLaborables - getDiasLaborablesRestantes();
 }
 
 function getHtmlHorasEstipuladas(horasEstipuladasEnSegundos, segundosEstipiladosEnJornadaIntensiva) {
@@ -171,10 +172,10 @@ function getHtmlDiferencia(diferenciaEnSegundos, diferenciaRealEnSegundos) {
 }
 
 function getHtmlHorasAlDia(segundosXDia, diasLaborablesRestantes, diferenciaRealEnSegundos) {
-	let horasAlDiaHastaFinDeMesEnSegundos = getHorasAlDiaHastaFinDeMesEnSegundos();
-	let segundosAcumulados = diferenciaRealEnSegundos / diasLaborablesRestantes;
-	let segundosDia = segundosXDia - segundosAcumulados;
-	let htmlHorasAlDia = getHoraHtmlFromSegundos(horasAlDiaHastaFinDeMesEnSegundos, false, true) + '<br>' + getHoraHtmlFromSegundos(segundosDia, false, true)
+	const horasAlDiaHastaFinDeMesEnSegundos = getHorasAlDiaHastaFinDeMesEnSegundos();
+	const segundosAcumulados = diferenciaRealEnSegundos / diasLaborablesRestantes;
+	const segundosDia = segundosXDia - segundosAcumulados;
+	const htmlHorasAlDia = getHoraHtmlFromSegundos(horasAlDiaHastaFinDeMesEnSegundos, false, true) + '<br>' + getHoraHtmlFromSegundos(segundosDia, false, true)
 	return htmlHorasAlDia;
 }
 
@@ -186,29 +187,28 @@ function getHtmlHoraSaldo(saldoCerditoEnSegundos, segundosConsumidosCerdito) {
 
 function main() {
 	// Datos extraidos
-    let horasEstipuladasEnSegundos = getHorasEstipuladasEnSegundos();
-    let horasEstipuladasAlFinalDelDiaEnSegundos = getHorasEstipuladasAlFinalDelDiaEnSegundos();
-    let diferenciaEnSegundos = getDiferenciaEnSegundos();
-    let diasLaborablesRestantes = getDiasLaborablesRestantes();
-    let saldoCerditoEnSegundos = getSaldoCerditoEnSegundos();
-    
+	const horasEstipuladasEnSegundos = getHorasEstipuladasEnSegundos();
+	const horasEstipuladasAlFinalDelDiaEnSegundos = getHorasEstipuladasAlFinalDelDiaEnSegundos();
+	const diferenciaEnSegundos = getDiferenciaEnSegundos();
+	const diasLaborablesRestantes = getDiasLaborablesRestantes();
+	const saldoCerditoEnSegundos = getSaldoCerditoEnSegundos();
+
 	// Datos calculados
-    let diasLaborables = getDiasLaborables(horasEstipuladasEnSegundos);
-    let segundosARestarParaJornadaIntensiva = getSegundosARestarParaJornadaIntensiva(diasLaborables, saldoCerditoEnSegundos);
-    let diasTrabajados = diasLaborables - diasLaborablesRestantes;
-    let segundosEstipiladosEnJornadaIntensiva = horasEstipuladasEnSegundos - segundosARestarParaJornadaIntensiva; // Dato modificado
-    let descuentoSegundosCerditoAlDia = segundosARestarParaJornadaIntensiva / diasLaborables;
-	let segundosXDia = segundosEstipiladosEnJornadaIntensiva / diasLaborables;
-	let segundosEstipuladosAlFinalDelDiaEnJornadaIntensiva = diasTrabajados * segundosXDia;
-    let segundosConsumidosCerdito = getSegundosConsumidosCerdito(saldoCerditoEnSegundos, diasTrabajados, descuentoSegundosCerditoAlDia); // Dato modificado
-    let diferenciaRealEnSegundos = diferenciaEnSegundos + segundosConsumidosCerdito; // Dato modificado
+	const diasLaborables = getDiasLaborables(horasEstipuladasEnSegundos);
+	const segundosARestarParaJornadaIntensiva = getSegundosARestarParaJornadaIntensiva(diasLaborables, saldoCerditoEnSegundos);
+	const diasTrabajados = diasLaborables - diasLaborablesRestantes;
+	const segundosEstipiladosEnJornadaIntensiva = horasEstipuladasEnSegundos - segundosARestarParaJornadaIntensiva; // Dato modificado
+	const descuentoSegundosCerditoAlDia = segundosARestarParaJornadaIntensiva / diasLaborables;
+	const segundosXDia = segundosEstipiladosEnJornadaIntensiva / diasLaborables;
+	const segundosConsumidosCerdito = getSegundosConsumidosCerdito(saldoCerditoEnSegundos, diasTrabajados, descuentoSegundosCerditoAlDia); // Dato modificado
+	const diferenciaRealEnSegundos = diferenciaEnSegundos + segundosConsumidosCerdito; // Dato modificado
 
 	// HTML's modificados
-	let htmlHorasEstipuladas = getHtmlHorasEstipuladas(horasEstipuladasEnSegundos, segundosEstipiladosEnJornadaIntensiva);
-	let htmlHorasEstipuladasAlFinalDelDia = getHtmlHorasEstipuladasAlFinalDelDia(horasEstipuladasAlFinalDelDiaEnSegundos, diasTrabajados, segundosXDia);
-	let htmlDiferenciaReal = getHtmlDiferencia(diferenciaEnSegundos, diferenciaRealEnSegundos);
-	let htmlHorasAlDia = getHtmlHorasAlDia(segundosXDia, diasLaborablesRestantes, diferenciaRealEnSegundos);
-	let htmlHorasSaldo = getHtmlHoraSaldo(saldoCerditoEnSegundos, segundosConsumidosCerdito);
+	const htmlHorasEstipuladas = getHtmlHorasEstipuladas(horasEstipuladasEnSegundos, segundosEstipiladosEnJornadaIntensiva);
+	const htmlHorasEstipuladasAlFinalDelDia = getHtmlHorasEstipuladasAlFinalDelDia(horasEstipuladasAlFinalDelDiaEnSegundos, diasTrabajados, segundosXDia);
+	const htmlDiferenciaReal = getHtmlDiferencia(diferenciaEnSegundos, diferenciaRealEnSegundos);
+	const htmlHorasAlDia = getHtmlHorasAlDia(segundosXDia, diasLaborablesRestantes, diferenciaRealEnSegundos);
+	const htmlHorasSaldo = getHtmlHoraSaldo(saldoCerditoEnSegundos, segundosConsumidosCerdito);
 
 	// Seteamos los HTML's
 	setHorasEstipuladas(htmlHorasEstipuladas);
@@ -219,7 +219,9 @@ function main() {
 	setAclaracion(descuentoSegundosCerditoAlDia, segundosXDia);
 }
 
-(function() {
-    'use strict';
-    main();
+(function () {
+	'use strict';
+	if (esEpocaDeJornadaIntensiva()) {
+		main();
+	}
 })();
